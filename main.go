7c7bf1e431f93e623d3b9a67e61f043b2ce26590
Jessica Ward <jessicawardiom@gmail.com>
2019-01-27 00:20:13 +0000

    Build the driver route handler
    Navigate to localhost:8080/driver/2 for the best driver ;)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,11 +17,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func driverHandler(w http.ResponseWriter, r *http.Request) {
+	drivers := data.GetAllDrivers()
+
+	//ID = every character after :8080/driver/.
+	//err = error stemming from type conversion
+	id, err := strconv.Atoi(r.URL.Path[len("/driver/"):])
+
+	//Type conversion error checking
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Gets driver by id (rly inefficient atm, I know)
+	driver := drivers[id]
+
+	//sends basic HTML as HTTP response text (uses string interpolation to insert driver name and number)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", driver.Name, driver.Number)
+}
+
 func main() {
+	//Routes
+	//todo: move these to a routing packge
+	http.HandleFunc("/driver/", driverHandler)
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+//NOTE: this is temporary, used for getting driver/team IDs etc
 func testData() {
 	//Terminal padding
 	fmt.Println()
